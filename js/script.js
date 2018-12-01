fa89782f7b9cb3ac7bf4d51d3e5a7b103d873060
Stephen Carroll-Keene <stephen.carrollkeene@gmail.com>
2018-12-01 18:29:01 +0000

    added about.html & contact.html

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 function bookSearch() {
     let search = document.getElementById('search').value;
-
     document.getElementById('results').innerHTML = "";
-
-    console.log(search);
 
     $.ajax({
         url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
